7cc50ba218ed7f6ec2be41dc6a8c789d3ab42567
ya-hya <yahya123mars@gmail.com>
2024-04-01 23:23:37 +0000

    last

--- js/script.js
+++ js/script.js
@@ -3,75 +3,62 @@ let menubtn = document.getElementById("menuBtn");
 let menulinkes = document.getElementById("menulinkes");
 let links = document.getElementsByTagName("li");
 
-// Slider Vars
-const sliderCont = document.getElementById("sliderContId");
-const slider = document.getElementById("sliderId");
-const cards = document.querySelectorAll(".card");
-let currentIndex = 0;
-let prevIsClicked = 0;
-let nextIsClicked = 0;
-let sliderContWidth = sliderCont.offsetWidth;
-let sliderWidth = slider.offsetWidth;
-const cardWidth = cards[0].offsetWidth + 20;
-const nextbtns = document.getElementsByClassName("next");
-const prevbtns = document.getElementsByClassName("prev");
-
 // Navbar
-menubtn.onclick = ()=>{
-    if (window.innerWidth < 646) {
-        menulinkes.classList.toggle("isActive");
-    }
-}
+menubtn.onclick = () => {
+  if (window.innerWidth < 646) {
+    menulinkes.classList.toggle("isActive");
+  }
+};
 
 for (let i = 0; i < links.length; i++) {
-    if (window.innerWidth < 646) {
-        links[i].onclick = () => menubtn.onclick();
-    }
+  if (window.innerWidth < 646) {
+    links[i].onclick = () => menubtn.onclick();
+  }
 }
 
-  
-// Next button
+// Swiper
 
-for (let i = 0; i < nextbtns.length; i++) {
-    nextbtns[i].onclick =  function () {
-        if (!nextIsClicked) {
-            if (currentIndex >= 0) return;
-            nextIsClicked = 1;
-            let sliderLeft = slider.offsetLeft * -1;
-            if (sliderLeft < cardWidth)
-                currentIndex += sliderLeft;
-            else
-                currentIndex += cardWidth;
-            slider.style.left = `${currentIndex}px`;
-            setTimeout(()=>{
-                nextIsClicked = 0;
-            },200);
-        }
-    };
-    
-}
+var swiper = new Swiper(".swiper-container", {
+  direction: "horizontal",
+  loop: true,
 
-// Previous button
+  breakpoints: {
+    280: {
+      slidesPerView: 1,
+      spaceBetween: 5,
+    },
+    350: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+    480: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
 
-for (let i = 0; i < prevbtns.length; i++) {
+    640: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
 
-    prevbtns[i].onclick = function () {
-        if (!prevIsClicked) {
-            prevIsClicked = 1;
-            let sliderLeft  = slider.offsetLeft;
-            if (sliderLeft < 0) sliderLeft *= -1;
-            let sliderRight = (sliderWidth - sliderLeft);
-            if (sliderRight <= sliderContWidth) return;
-            if (sliderRight - sliderContWidth < cardWidth)
-                currentIndex -= (sliderRight - sliderContWidth);
-            else
-                currentIndex -= cardWidth;
-            slider.style.left = `${currentIndex}px`;
-            setTimeout(()=>{
-                prevIsClicked = 0;
-            },200);
-        }
-    };
-}
-  
+    991: {
+      slidesPerView: 4,
+      spaceBetween: 35,
+    },
 
+    1100: {
+      slidesPerView: 5,
+      spaceBetween: 40,
+    },
+  },
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+});
